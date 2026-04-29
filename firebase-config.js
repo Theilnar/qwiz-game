d901ba8e505a40e7119cc36a1684f87cfe511456
Theilnar <ilnar.muhametgaleev@gmail.com>
2026-04-29 21:38:44 +0300

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.SVOYAK_FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAqhO-4KKM_R7L1tX7fZ991B9JJAiWWl8A",
+    authDomain: "planning-with-ai-141de.firebaseapp.com",
+    projectId: "planning-with-ai-141de",
+    storageBucket: "planning-with-ai-141de.firebasestorage.app",
+    messagingSenderId: "908031563425",
+    appId: "1:908031563425:web:38bf02e8a075f9da06c2e0"
+};
